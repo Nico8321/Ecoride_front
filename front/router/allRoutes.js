a145931e0e4d81c0de9c67b299f728a90ac0c971
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-06-08 12:46:01 +0200

    Ajout des pages signin & signup

--- front/router/allRoutes.js
+++ front/router/allRoutes.js
@@ -1,7 +1,12 @@
 import Route from "./route.js";
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html", []), new Route("/recherche", "Recherche", "/pages/recherche.html", [])];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html", []),
+  new Route("/recherche", "Recherche", "/pages/recherche.html", []),
+  new Route("/signup", "Inscription", "/pages/signup.html", []),
+  new Route("/signin", "Connexion", "/pages/signin.html", []),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
