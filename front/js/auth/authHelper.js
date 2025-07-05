b2e4d793d694c11d221a27e2ac063439b46c8a7e
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-07-05 15:13:02 +0200

    chore: activation de la vérification du token JWT dans authHelper.js

--- front/js/auth/authHelper.js
+++ front/js/auth/authHelper.js
@@ -1,22 +1,20 @@
 export async function verifyToken() {
-  /*
   const token = sessionStorage.getItem("token");
+  if (!token) return false;
+
   try {
     const response = await fetch(`${apiUrl}/auth/verify`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP: ${response.status}`);
-    }
-    return true;
+
+    const result = await response.json();
+    return result.success === true;
   } catch (error) {
     console.error(`Erreur HTTP: ${error.message}`);
     return false;
-  }*/
-
-  return true;
+  }
 }
 
 export async function isLoggedIn() {
