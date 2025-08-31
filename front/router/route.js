1e2960c5efae83cfab14f8997b577a9faff217a4
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-08-31 14:50:22 +0200

    feat(us12): gestion des accès par rôle (staff) - routes staff protégées (authorize=false, role [2]) - vérif d’auth via isLoggedIn + session user - redirection post-login selon role_id (staff vs utilisateur)

--- front/router/route.js
+++ front/router/route.js
@@ -1,9 +1,10 @@
 export default class Route {
-  constructor(url, title, pathHtml, authorize, pathJS = "") {
+  constructor(url, title, pathHtml, authorize, pathJS = "", role = null) {
     this.url = url;
     this.title = title;
     this.pathHtml = pathHtml;
     this.pathJS = pathJS;
     this.authorize = authorize;
+    this.role = role;
   }
 }
