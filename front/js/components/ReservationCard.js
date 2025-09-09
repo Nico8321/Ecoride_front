5c6f6e3b40f480a280b7544665bdeda76bf9e78a
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-09-09 11:13:14 +0200

    fix(monCompte.js): ajout d'un border-warnig-subtle sur les cards des réservations annulées pour améliorer l'affichage

--- front/js/components/ReservationCard.js
+++ front/js/components/ReservationCard.js
@@ -13,6 +13,8 @@ export function createReservationCard(reservation, destination) {
     card.className = "card shadow-sm w-100 rounded-4 overflow-hidden mb-2 bg-warning-subtle border-start border-5 border-warning";
   } else if (reservation.statut == "en attente") {
     card.className = "card shadow-sm w-100 rounded-4 overflow-hidden mb-2 bg-warning-subtle border-start border-5 border-info";
+  } else if (reservation.statut == "annule") {
+    card.className = "card shadow-sm w-100 rounded-4 overflow-hidden mb-2 bg-warning-subtle border-start border-5 border-warning-subtle";
   }
   card.innerHTML = `
 <div class="card-body d-flex flex-column flex-md-row gap-3">
