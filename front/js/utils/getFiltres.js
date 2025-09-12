e7fca662447022c1727a32835a251dbe5bf9b3d6
Nico8321 <189213788+Nico8321@users.noreply.github.com>
2025-09-12 11:20:57 +0200

    fix(recherche): affichage du prochain covoiturage dispo si aucun trajet trouvé
    - ajout getFiltresLight() pour recherche élargie
    - maj rechercherCovoiturages()

--- front/js/utils/getFiltres.js
+++ front/js/utils/getFiltres.js
@@ -22,3 +22,25 @@ export function getFiltres() {
 
   return filtres;
 }
+export function getFiltresLight() {
+  const donnees = ["depart", "arrivee"];
+  const filtres = {};
+
+  donnees.forEach((key) => {
+    const input = document.getElementById(key);
+    if (input?.value) {
+      filtres[key] = input.value;
+    }
+  });
+  const energieChecked = document.querySelector('input[name="radioEnergie"]:checked');
+  if (energieChecked) {
+    filtres["energie"] = energieChecked.id;
+  }
+  const userData = sessionStorage.getItem("user");
+  if (userData) {
+    const user = JSON.parse(userData);
+    filtres["userId"] = user.id;
+  }
+
+  return filtres;
+}
